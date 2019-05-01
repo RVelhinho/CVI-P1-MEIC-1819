70b946a6a2d8e45efa6ddf5f5da543f2f21bb8ca
João Pina <joaomfpina@tecnico.ulisboa.pt>
2019-05-01 23:12:31 +0100

    Update objectNumber.m

--- objectNumber.m
+++ objectNumber.m
@@ -1,19 +1,21 @@
 clear all, close all
 
-thr = 200;
-minArea = 20;
-
 img = imread('Moedas1.jpg');
+imshow(img);
 
-figure, imshow(img);
+g = rgb2gray(img);
+imshow(g);
 
-se = strel('disk', 3);
-bw1 = img < 200;
-figure,imshow(bw1);
+%imhist(g);
 
-%bw2 = imclose(bw1, se);
-%imshow(bw2);
+bw = g > 80;
+%imshow(bw);
 
-%[lb num] = bwlabel(bw2);
-%regionProps = regionprops(lb, 'area', 'FilledImage', 'Centroid');
-%inds = find([regionProps.Area]>minArea);
+se = strel('disk',4);
+afterOpening = imopen(bw, se);
+%imshow(afterOpening);
+
+afterClosing = imclose(afterOpening, se);
+%imshow(afterClosing);
+
+[L, num] = bwlabel(~afterClosing,4);
